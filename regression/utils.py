8e2be36aba4efa8410b8b99b42e8ec10707ec117
Egor Rakcheev <egor.rakcheev@tum.de>
2025-06-14 00:04:22 +0200

    Improved folder structure

---
+++ regression/utils.py
@@ -0,0 +1,10 @@
+
+
+def test(model,test_data_path):
+    ''' Test a certain model based on fixed regression metrics ...'''
+    # TODO
+    # mae = ...
+    # r2 = ...
+    # mse = ...
+    # save plot of predictions to results/figures/
+    return None

be5a3f487fc0be6ef030fa0fad0ed6b64c0552f9
Khalil Chikhaoui <chikhaou@selene.aim.cit.tum.de>
2025-07-31 20:12:08 +0200

    tabpfn relevant files

---
+++ classification/tabpfn/run_icl_classification.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=classification
+#SBATCH --output=outfiles3/classification.out
+#SBATCH --error=outfiles3/classification.err
+##SBATCH --partition=universe,asteroids
+##SBATCH --qos=master-queuesave
+#SBATCH --time=08:00:00
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=64G
+
+ml python/anaconda3
+
+echo "Starting Classification job with SLURM_JOB_ID: $SLURM_JOB_ID"
+
+# Set Python path to find modules
+export PYTHONPATH=/vol/miltank/projects/practical_sose25/in_context_learning:$PYTHONPATH
+
+python -u train_icl_classification.py
+
+echo "Classification job completed with SLURM_JOB_ID: $SLURM_JOB_ID"
